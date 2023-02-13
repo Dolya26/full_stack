973807c3fde06bc82e72271382318ec20928c027
SultanBaialin <baialinovsultan@gmail.com>
2023-02-13 19:33:49 +0600

    13.02.23

---
+++ product/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from category.models import Category, CountryCategory
+from ckeditor.fields import RichTextField
+
+User = get_user_model()
+
+
+class Product(models.Model):
+    STATUS_CHOISES =(
+        ('in_stock', 'активен'),
+        ('out_of_stock', 'неактивен')
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    country_category = models.ForeignKey(CountryCategory, related_name='country', on_delete=models.RESTRICT)
+
+    city = models.CharField(max_length=60)
+    address = models.CharField(max_length=60)
+    house_number = models.SmallIntegerField()
+    flat_number = models.SmallIntegerField()
+
+    guests = models.SmallIntegerField()
+    rooms = models.SmallIntegerField()
+    beds = models.SmallIntegerField()
+    bathrooms = models.SmallIntegerField()
+
+    wifi = models.BooleanField()
+    fridge = models.BooleanField()
+    air_conditioner = models.BooleanField()
+    tv = models.BooleanField()
+    pool = models.BooleanField()
+    furniture = models.BooleanField()
+    washing = models.BooleanField()
+    medicine = models.BooleanField()
+    kitchen = models.BooleanField()
+
+    title = models.CharField(max_length=150)
+    description = RichTextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    stock = models.CharField(choices=STATUS_CHOISES, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductImages(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='images/')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+
---
+++ product/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from .models import Product, ProductImages
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = '__all__'
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    owner_email = serializers.ReadOnlyField(source='owner.email')
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner_email = serializers.ReadOnlyField(source='owner.email')
+    owner = serializers.ReadOnlyField(source='owner.id')
+    # reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        product = Product.objects.create(**validated_data)
+        images_data = request.FILES.getlist('images')
+        for image in images_data:
+            ProductImages.objects.create(image=image, product=product)
+        return product
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        for k, v in validated_data.items():
+            setattr(instance, k, v)
+        instance.save()
+        instance.images.all().delete()
+        images_data = request.FILES.getlist('images')
+        for image in images_data:
+            ProductImages.objects.create(image=image, product=instance)
+        return instance
+
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['images'] = ProductImageSerializer(instance.images.all(), many=True).data
+        # repr['reviews'] = ReviewCreateSerializer(instance.reviews.all(), many=True).data
+
+        return repr
